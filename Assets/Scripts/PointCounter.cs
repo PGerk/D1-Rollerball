ce2046a7f1304645251a39c38d9d6a1831c441d0
Patrick Gerk <patrick.gerk@ruhr-uni-bochum.de>
2023-07-11 11:31:59 +0200

    Working Item counter

--- Assets/Scripts/PointCounter.cs
+++ Assets/Scripts/PointCounter.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointCounter : MonoBehaviour
 {
 
     int points = 0;
+    TextMeshProUGUI pointCount;
 
     public void AddPoint()
     {
         points++;
-        Debug.Log("Point Collected! Current Points: " + points);
+        pointCount.text = points.ToString();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointCount = GameObject.FindGameObjectWithTag("ItemsFound").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+        pointCount.text = "0";
     }
 
     // Update is called once per frame
