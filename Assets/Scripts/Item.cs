12e048e3f4c0f61cab82cd79fd84d0853b3c2a1f
Patrick Gerk <patrick.gerk@ruhr-uni-bochum.de>
2023-07-14 15:02:47 +0200

    Added Item effects

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     TextHandling textHandling;
+    public GameObject itemParticlePrefab;
     private void Awake()
     {
         textHandling = FindObjectOfType<TextHandling>();
     }
     private void OnTriggerEnter()
     {
+        Instantiate(itemParticlePrefab, transform.position, Quaternion.identity);
         textHandling.AddPoint();
         Destroy(gameObject);
     }
